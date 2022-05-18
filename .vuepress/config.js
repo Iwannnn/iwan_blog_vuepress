7eb23cb833309cae6cbe625d5fced367aef8cd27
iwannnn <iwannnn1206@163.com>
2022-05-18 23:00:55 +0800

    markdown it katex

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -252,8 +252,15 @@ module.exports = {
 		['@vuepress-reco/vuepress-plugin-pagation', {}],
 		['@vuepress-reco/vuepress-plugin-loading-page', {}]
 	],
-	extendMarkdown(md) {
-		md.set({ html: true });
-		md.use(require("markdown-it-katex"));
-	},
+	markdown: {
+		// markdown-it-anchor 的选项
+		anchor: { permalink: false },
+		// markdown-it-toc 的选项
+		toc: { includeLevel: [1, 2] },
+		extendMarkdown: md => {
+			// 使用更多的 markdown-it 插件!
+			md.set({ html: true });
+			md.use(require("markdown-it-katex"));
+		}
+	}
 }
